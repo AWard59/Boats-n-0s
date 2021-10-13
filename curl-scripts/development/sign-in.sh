e34bdd0472cae9c15f533629205e5a69c4d6cb0a
AWard59 <iamsancho5990@hotmail.com>
2021-10-12 20:34:38 -0700

    added change PW curl script, included production versions

--- curl-scripts/development/sign-in.sh
+++ curl-scripts/development/sign-in.sh
@@ -1,4 +1,4 @@
-# EMAIL= PASSWORD= sh curl-scripts/sign-in.sh
+# EMAIL= PASSWORD= sh curl-scripts/development/sign-in.sh
 
 curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
   --include \
